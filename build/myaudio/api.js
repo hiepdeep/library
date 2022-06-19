d30464eba0efec5df3c922890e3e274ccc2c6d15
HiepDeep <vanhiepofficial@gmail.com>
2022-06-19 21:22:55 +0700

    1

---
+++ build/myaudio/api.js
@@ -0,0 +1,42 @@
+let tracks = [
+	{
+		"url": "https://zingmp3.vn/bai-hat/Lac-Vao-Trong-Mo-SimonC-WUY/ZZ9F9O80.html",
+		"key": "ZHxHyZZFcXsJLzztHyDnZHyLCWQhWbHJu"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Cho-Qua-Lau-Le-Bao-Binh/ZZDO87CU.html",
+		"key": "LHJmTZZdvHJdmkFyHtbHLntLCQpCWkEpb"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Chi-La-Da-Tung-Thanh-Dat/ZZDUIB87.html",
+		"key": "ZmxmTLZVFkFHJVRymtDGZHyZXQQgpbBFF"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Xin-Loi-Nguoi-Anh-Yeu-Chau-Khai-Phong/ZW6D77EF.html",
+		"key": "knxHTZmsQnCFgBRyntvmZmTZgQpgpbzdu"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/So-Ta-Mat-Nhau-Chau-Khai-Phong-ACV/ZOFEOCIO.html",
+		"key": "LncmyZZGLBbsFQkTHyFHZmTLXQphWDzWh"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Tha-La-Khai-Dang/ZZ8D9UZI.html",
+		"key": "ZmJmyZkbNzRxNHWynybGZmyZCWpCQDzah"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Nguoi-Yeu-Toi-Lanh-Lung-Sat-Da-Mr-Siro/ZZ6IW7OU.html",
+		"key": "LnJHykLFAQJNLJctGtFnZmtkCpphQFzRS"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Yeu-Nham-Nguoi-Khac-Viet/ZW9BUIEO.html",
+		"key": "LncmyZnaxmhdlQRymtFmZntZhWphpvWkk"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Nhuong-Dieu-Uoc-Cho-Em-Cao-Tung-Anh/ZW679FWD.html",
+		"key": "LHJmTkmszXauSXEyGtDnLHtLgWpCQvpQs"
+	},
+	{
+		"url": "https://zingmp3.vn/bai-hat/Buon-Cua-Anh-K-ICM-Dat-G-Masew/ZW8WD9BE.html",
+		"key": "LGJGyZnNhzXdidAymyDmkGyZhWphQbpNB"
+	}
+]
